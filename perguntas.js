cc9dc99c701081a0e6aaf69a0c71cf82f5ea3779
elisama <132621154+eppires@users.noreply.github.com>
2024-10-24 14:22:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é a Linha do Equador e quais continentes ela atravessa?',
+    'A Linha do Equador é uma linha imaginária que divide a Terra em hemisférios norte e sul, atravessando a América do Sul, África e Ásia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa HTML?',
+    'HyperText Markup Language'
+)
+
+criaCartao(
+    'Física',
+    'O que é a gravidade?',
+    'É a força que atrai os objetos para o centro da Terra'
+)
+
+criaCartao(
+    'Química',
+    'O que é a tabela periódica?',
+    'É um quadro que organiza os elementos químicos de acordo com suas propriedades.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a função dos pulmões no corpo humano?',
+    'Os pulmões são responsáveis pela troca de oxigênio e gás carbônico no corpo'
+)
+
+criaCartao(
+    'História',
+    'Quando ocorreu a Revolução Francesa?',
+    'Em 1789'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o deserto mais seco do mundo?',
+    'O Deserto do Atacama'
+)
+
+criaCartao(
+    'Física',
+    'O que é a velocidade da luz?',
+    'É a velocidade com que a luz se propaga no vácuo, cerca de 300.000 km/s'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa CSS?',
+    'Cascading Style Sheets'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu “Dom Quixote”?',
+    'Miguel de Cervantes'
+)
